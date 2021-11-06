069c7c6d81e1e15b0fd79b155d5dea65f26c5306
Xurxo Breogan Costa Lopez <xlopez3@gatech.edu>
2021-11-06 11:47:13 +0100

    let's define explicitly namespaces, following good practices

--- infinites_test.cc
+++ infinites_test.cc
@@ -7,15 +7,13 @@
 
 #define INF  ((unsigned) ~0)
 
-using namespace std;
-
 int main ()
 {
-    std::cout << "- infinity int from ((unsigned) ~0)" << endl;
-    std::cout << INF << endl;
-    std::cout << "- infinity float from limits' numeric_limits<float>::infinity()" << endl;
-    std::cout << std::numeric_limits<float>::infinity() << endl;
-    std::cout << "- Is infinity float > infinity int?" << endl;
+    std::cout << "- infinity int from ((unsigned) ~0)" << std::endl;
+    std::cout << INF << std::endl;
+    std::cout << "- infinity float from limits' numeric_limits<float>::infinity()" << std::endl;
+    std::cout << std::numeric_limits<float>::infinity() << std::endl;
+    std::cout << "- Is infinity float > infinity int?" << std::endl;
     std::cout << std::boolalpha;
-    std::cout << bool(std::numeric_limits<float>::infinity() > INF) << endl;
+    std::cout << bool(std::numeric_limits<float>::infinity() > INF) << std::endl;
 }
